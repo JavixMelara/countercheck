aa2081e41f3b2e817024184b0f1d4566ace3b9a4
javix <javixmela@hotmail.com>
2025-08-29 20:56:36 -0600

    actualizar

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 export default async function handler(req, res) {
   try {
-    // Incrementa el contador en CountAPI
-    await fetch("https://api.countapi.xyz/hit/micontador/instagram");
+    // Incrementa un contador único en CountAPI
+    // La primera vez que se use, se crea automáticamente
+    await fetch("https://api.countapi.xyz/hit/javier/instapost1?amount=1");
 
-    // Redirige a Instagram
+    // Redirige a tu publicación de Instagram
     res.writeHead(302, {
       Location: "https://www.instagram.com/p/DNfyp6yu-ag/?igsh=cDJpdHBrZnVodGt4"
     });
